9dcc2c7d54afd4328982034925420df305b1b11d
19016939 <pargatgill1289@gmail.com>
2020-01-29 14:02:20 -0500

    lab3 done

--- app/src/main/java/com/soogreyhounds/soogreyhoundsmobile/MainActivity.java
+++ app/src/main/java/com/soogreyhounds/soogreyhoundsmobile/MainActivity.java
@@ -1,7 +1,14 @@
 package com.soogreyhounds.soogreyhoundsmobile;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +16,40 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button photoDetailButton = findViewById(R.id.photoDetailButton);
+        photoDetailButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getBaseContext(), PhotoDetailActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.activity_main, menu);
+        return true;
     }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.login:
+                Intent loginIntent = new Intent(this, LoginActivity.class);
+                startActivity(loginIntent);
+                return true;
+            case R.id.create_account:
+                Intent createAccountIntent = new Intent(this, CreateAccountActivity.class);
+                startActivity(createAccountIntent);
+                return true;
+            case R.id.edit_profile:
+                Intent editProfileIntent = new Intent(this, EditProfileActivity.class);
+                startActivity(editProfileIntent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
 }
